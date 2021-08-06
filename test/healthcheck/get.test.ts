bc4b49e0a1fec4fdf1affce4eb8de81f9de957aa
Kolya Venturi <kolya.venturi@enx3s.com>
2021-08-06 14:37:15 -0700

    Add healthcheck

--- test/healthcheck/get.test.ts
+++ test/healthcheck/get.test.ts
@@ -1,8 +1,21 @@
 import test from 'ava';
+import sinon, {stub} from 'sinon';
+import proxyquire from 'proxyquire';
 
-import {healthcheck} from '../../src/healthcheck/get';
+const prom: sinon.SinonStub = stub().resolves();
+const {healthcheck} = proxyquire.noCallThru()('../../src/healthcheck/get', {
+  '..db': {
+    db: {
+      scan: stub().returns(prom),
+    },
+  },
+});
 
-test('returns a 200 ok response', async t => {
+test.beforeEach(() => {
+  prom.reset();
+});
+
+test('returns a 200 ok response if the DB call passes', async t => {
   const result = await healthcheck({} as AWSLambda.APIGatewayProxyEvent);
 
   t.deepEqual(result, {
@@ -10,3 +23,14 @@ test('returns a 200 ok response', async t => {
     body: 'page ok',
   });
 });
+
+test('returns a 503 response if the DB call fails', async t => {
+  prom.rejects();
+  const result = await healthcheck({} as AWSLambda.APIGatewayProxyEvent);
+
+  t.deepEqual(result, {
+    statusCode: 503,
+    body: '503 Service Unavailable',
+  });
+});
+
